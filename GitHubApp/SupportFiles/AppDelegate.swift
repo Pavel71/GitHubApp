7ca19e255df85c9966bee85a036c2bd5834e1c95
Pavel71 <raz_3@mail.ru>
2020-08-24 13:54:16 +0300

    Create root Project

---
+++ GitHubApp/SupportFiles/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  GitHubApp
+//
+//  Created by Павел Мишагин on 24.08.2020.
+//  Copyright © 2020 Павел Мишагин. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window : UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    root()
+    
+    return true
+  }
+  
+  private func root() {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let navController = UINavigationController(rootViewController: ViewController())
+    window?.rootViewController = navController
+    window?.makeKeyAndVisible()
+  }
+
+
+
+}
+
